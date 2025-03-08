df58529e587f7db3f78ea26d7a64c165ca093f86
ehj22 <ehj22@mit.edu>
2025-03-08 12:38:54 -0500

    Update SM.py

--- SM.py
+++ SM.py
@@ -1,7 +1,7 @@
 ## Relevant Attributes for Plane Vanilla ##
 
-c = 0.15 # units: m
-c_h = 0.06 # units: m
+c = 0.15 # cord
+c_h = 0.06 # cord of the horizontal tail
 b = 1.5 # units: m
 b_h = 0,4 # units: m
 f_e = 0.6
@@ -9,6 +9,8 @@ l_h = 0.65 # units: m
 CLw_nom = 0.65
 CMw_nom = -0.15
 
+S = c*b
+S_h = c_h*b_h
 ## Calculate Xcg/c ##
 
 Xcg_c = 1/4 - (CMw_nom/CLw_nom)
